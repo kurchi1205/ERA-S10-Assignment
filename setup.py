610d26807f95bef4c57ce7f3a9fe00c067574d94
Prerana <prerana@hellosivi.com>
2023-07-14 00:07:05 +0530

    new setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='CustomResnet',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/kurchi1205/ERA-S10-Assignment.git',
     author='prerana',
     author_email='prerana.chaks@gmail.com',
     description='ERA-S10-Assignment',
-    packages=find_packages(),    
+    packages=find_packages(),  
+    install_requires=['torch', 'tqdm', 'torchsummary', 'albumentations']  
 )
