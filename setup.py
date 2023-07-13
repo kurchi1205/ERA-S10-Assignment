2f9cb471dd84f247278bd3cda2ae0d0fb7b01680
Prerana <prerana@hellosivi.com>
2023-07-13 23:41:09 +0530

    package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CustomResnet',
+    version='1.0.0',
+    url='https://github.com/kurchi1205/ERA-S10-Assignment.git',
+    author='prerana',
+    author_email='prerana.chaks@gmail.com',
+    description='ERA-S10-Assignment',
+    packages=find_packages(),    
+)
